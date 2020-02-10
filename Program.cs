ad9e07b7e7d6a310d3569fb14cc5cb223949d02b
Igor Dias <igordias.jd@gmail.com>
2020-02-10 18:26:47 -0300

    Initial Project Settings

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KAMS_Keyboard_And_Mouse_Share
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
