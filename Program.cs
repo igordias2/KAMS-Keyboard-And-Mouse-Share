ac48759ada885c61a44a0f148bba98d50906ed36
Igor Dias <igordias.jd@gmail.com>
2020-02-10 21:33:24 -0300

    cleanup

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+
 namespace KAMS_Keyboard_And_Mouse_Share
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        
+            // while (true)
+            // {
+            //     var key =  Console.ReadKey();
+            //     Console.WriteLine(key.Key);
+            // }
         }
     }
 }
+
